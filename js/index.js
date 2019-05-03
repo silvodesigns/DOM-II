4c2cca6d7c7cd015d4ad39f52a6ea565e10e9987
Kevin-S <kevsilvo@gmail.com>
2019-05-03 19:14:28 -0400

    first-commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const navigationList = document.querySelectorAll(".nav-link");
+console.log(navigationList);
+navigationList.forEach(function(element){
+    element.addEventListener("mouseover", function(event){
+            event.target.style.color = "yellow";
+    });
+
+    element.addEventListener("mouseleave", function(event){
+        event.target.style.color = "black";
+    });
+
+});
+
