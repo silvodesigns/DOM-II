b463b8e95ba402cb643ebe73e0322c4cd2e22e27
Kevin-S <kevsilvo@gmail.com>
2019-05-03 19:49:35 -0400

    4 events

--- js/index.js
+++ js/index.js
@@ -1,14 +1,55 @@
 // Your code goes here
 const navigationList = document.querySelectorAll(".nav-link");
-console.log(navigationList);
 navigationList.forEach(function(element){
+    //MOUSEOVER EVENT
     element.addEventListener("mouseover", function(event){
             event.target.style.color = "yellow";
     });
-
+    //MOUSELEAVE EVENT
     element.addEventListener("mouseleave", function(event){
         event.target.style.color = "black";
     });
+
+});
+//SPELL FUN BUS AND INSET ON PAGE
+document.addEventListener('keydown', logKey);
+var container = document.querySelector(".fun-bus-writting");
+function logKey(e) {
+    const funBus =[];
+        if(e.code == "KeyF"){
+            funBus.push("F");
+
+        }
+        if(e.code == "KeyU"){
+            funBus.push("U");
+
+        }
+        if(e.code == "KeyN"){
+            funBus.push("N");
+
+        }
+        if(e.code == "KeyB"){
+            funBus.push("B");
+
+        }
+        if(e.code == "KeyS"){
+            funBus.push("S");
+
+        }
+  funBus.forEach(function(element){
+      if(element !== ""){
+          container.innerHTML += `<h1>${element}</h1>`;
+
+      }
+
+  });
+}
+
+var bodyWindow = document.querySelector("body");
+window.addEventListener('scroll', function(e) {
+   if(e.isTrusted == true){
+       bodyWindow.style.backgroundColor ="pink";
+   }
 
 });
 
